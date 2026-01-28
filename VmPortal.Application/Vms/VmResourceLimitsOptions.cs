577cca835f884cbd8d40d171c3cf7ce9fc4b7227
Job Dirks <26822391+JobDirks@users.noreply.github.com>
2026-01-28 21:55:11 +0100

    feat(vm): template-driven defaults and live resource validation on Create VM

--- VmPortal.Application/Vms/VmResourceLimitsOptions.cs
+++ VmPortal.Application/Vms/VmResourceLimitsOptions.cs
@@ -3,5 +3,9 @@ namespace VmPortal.Application.Vms
     public class VmResourceLimitsOptions
     {
         public int MaxDiskGiB { get; set; } = 120;
+
+        public int MaxCpuCores { get; set; } = 8;
+
+        public int MaxMemoryMiB { get; set; } = 16384;
     }
 }
